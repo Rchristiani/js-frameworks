1997c9e51aae37ac0b4b7f3b0dfce318b9785005
Ryan Christiani <ryan@ryanchristiani.com>
2015-06-13 14:36:41 -0400

    Finsihing up react example

--- react/script.js
+++ react/script.js
@@ -3,8 +3,9 @@
 var apiKey = 'pUaGTYo5';
 var apiUrl = 'https://www.rijksmuseum.nl/api/en/collection/?key=' + apiKey;	
 //Get info
-var getArtData = function() {
-	return $.get(apiUrl,{
+var getArtData = function(query) {
+	var query = query === undefined ? '' : query;
+	return $.get(apiUrl + '&q=' + query,{
 		dataType: 'jsonp'
 	});
 };
@@ -19,23 +20,35 @@ var Gallery = React.createClass({displayName: "Gallery",
 	componentDidMount: function() {
 		getArtData().then(function(res)  {
 			var art = res.artObjects;
-			art.map(function(el, i)  {
+			this.createItems(art);
+		}.bind(this), function(err) {
+			console.log(err);
+		});
+	},
+
+	componentWillReceiveProps: function(obj) {
+		this.createItems(obj.data);
+	},
+
+	createItems: function(art) {
+		this.state.items = [];
+		art.map(function(el, i)  {
+			if(el.webImage !== null) {
 				this.state.items.push(
 					React.createElement("div", {className: "art-item", key: el.id}, 
 						React.createElement("img", {src: el.webImage.url}), 
 						React.createElement("p", null, el.longTitle)
 					)
 				);
-				if(i === (art.length - 1)) {
-					this.setState({
-						items : this.state.items
-					})
-				}
-			}.bind(this));
-		}.bind(this), function(err) {
-			console.log(err);
-		});
+			}
+			if(i === (art.length - 1)) {
+				this.setState({
+					items : this.state.items
+				})
+			}
+		}.bind(this));
 	},
+
 	render: function() {
 		return (
 			React.createElement("div", null, this.state.items)
@@ -51,7 +64,12 @@ var HeaderComponent = React.createClass({displayName: "HeaderComponent",
 	},
 	searchArt: function(e) {
 		e.preventDefault();
-
+		getArtData(this.state.searchValue).then(function(res)  {
+			this.props.onSearch(res.artObjects);
+			this.setState({
+				searchValue: ''
+			});
+		}.bind(this));
 	},
 	onChange: function(e) {
 		this.setState({
@@ -63,8 +81,10 @@ var HeaderComponent = React.createClass({displayName: "HeaderComponent",
 			React.createElement("header", {className: "main-header"}, 
 				React.createElement("h1", null, "Rijks Museum"), 
 				React.createElement("div", {className: "search-box"}, 
-					React.createElement("input", {type: "text", value: this.state.searchValue, onChange: this.onChange}), 
-					React.createElement("input", {type: "submit", value: "search", onClick: this.searchArt})
+					React.createElement("form", {onSubmit: this.searchArt}, 
+						React.createElement("input", {type: "text", value: this.state.searchValue, onChange: this.onChange}), 
+						React.createElement("input", {type: "submit", value: "search"})
+					)
 				)
 			)
 		);
@@ -72,12 +92,23 @@ var HeaderComponent = React.createClass({displayName: "HeaderComponent",
 });
 
 var App = React.createClass({displayName: "App",
+	getInitialState: function() {
+		return {
+			data: []
+		};
+	},
+	searchHandler: function(data) {
+		this.setState({
+			data: data
+		});
+	},
 	render: function() {
+
 		return (
 			React.createElement("main", {className: "main-section"}, 
-				React.createElement(HeaderComponent, null), 
+				React.createElement(HeaderComponent, {onSearch: this.searchHandler}), 
 				React.createElement("section", {className: "art-work"}, 
-					React.createElement(Gallery, null)
+					React.createElement(Gallery, {data: this.state.data})
 				)
 			)
 		);
--- react/script.jsx
+++ react/script.jsx
@@ -3,8 +3,9 @@
 var apiKey = 'pUaGTYo5';
 var apiUrl = 'https://www.rijksmuseum.nl/api/en/collection/?key=' + apiKey;	
 //Get info
-var getArtData = function() {
-	return $.get(apiUrl,{
+var getArtData = function(query) {
+	var query = query === undefined ? '' : query;
+	return $.get(apiUrl + '&q=' + query,{
 		dataType: 'jsonp'
 	});
 };
@@ -19,23 +20,35 @@ var Gallery = React.createClass({
 	componentDidMount: function() {
 		getArtData().then((res) => {
 			var art = res.artObjects;
-			art.map((el, i) => {
+			this.createItems(art);
+		}, function(err) {
+			console.log(err);
+		});
+	},
+
+	componentWillReceiveProps: function(obj) {
+		this.createItems(obj.data);
+	},
+
+	createItems: function(art) {
+		this.state.items = [];
+		art.map((el, i) => {
+			if(el.webImage !== null) {
 				this.state.items.push(
 					<div className="art-item" key={el.id}>
 						<img src={el.webImage.url} />
 						<p>{el.longTitle}</p>
 					</div>
 				);
-				if(i === (art.length - 1)) {
-					this.setState({
-						items : this.state.items
-					})
-				}
-			});
-		}, function(err) {
-			console.log(err);
+			}
+			if(i === (art.length - 1)) {
+				this.setState({
+					items : this.state.items
+				})
+			}
 		});
 	},
+
 	render: function() {
 		return (
 			<div>{this.state.items}</div>
@@ -51,7 +64,12 @@ var HeaderComponent = React.createClass({
 	},
 	searchArt: function(e) {
 		e.preventDefault();
-
+		getArtData(this.state.searchValue).then(res => {
+			this.props.onSearch(res.artObjects);
+			this.setState({
+				searchValue: ''
+			});
+		});
 	},
 	onChange: function(e) {
 		this.setState({
@@ -63,8 +81,10 @@ var HeaderComponent = React.createClass({
 			<header className="main-header">
 				<h1>Rijks Museum</h1>
 				<div className="search-box">
-					<input type="text" value={this.state.searchValue} onChange={this.onChange}/>
-					<input type="submit" value="search" onClick={this.searchArt} />
+					<form onSubmit={this.searchArt}>
+						<input type="text" value={this.state.searchValue} onChange={this.onChange}/>
+						<input type="submit" value="search" />
+					</form>
 				</div>
 			</header>
 		);
@@ -72,12 +92,23 @@ var HeaderComponent = React.createClass({
 });
 
 var App = React.createClass({
+	getInitialState: function() {
+		return {
+			data: []
+		};
+	},
+	searchHandler: function(data) {
+		this.setState({
+			data: data
+		});
+	},
 	render: function() {
+
 		return (
 			<main className="main-section">
-				<HeaderComponent/>
+				<HeaderComponent onSearch={this.searchHandler}/>
 				<section className="art-work">
-					<Gallery/>
+					<Gallery data={this.state.data}/>
 				</section>
 			</main>
 		);
